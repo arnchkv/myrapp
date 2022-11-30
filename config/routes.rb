ac8d4aa5af5b119ab0092c6a59b0deb4361921c0
Arnav <arnchkv@gmail.com>
2022-11-30 14:47:30 +0530

    root to student index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :todos
   resources :students
+
+  root "students#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
